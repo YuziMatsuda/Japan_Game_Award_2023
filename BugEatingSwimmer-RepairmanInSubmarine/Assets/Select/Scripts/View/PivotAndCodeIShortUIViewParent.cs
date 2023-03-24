bb94fe810fe985942b35ae9b09861f60d9925d35
ktgitfrog333 <paraysis+git@gmail.com>
2023-03-24 21:25:43 +0900

    ▼UI（α版） ・ユニット１：ヘッドの実装と連携

---
+++ BugEatingSwimmer-RepairmanInSubmarine/Assets/Select/Scripts/View/PivotAndCodeIShortUIViewParent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Select.View
+{
+    /// <summary>
+    /// ビュー
+    /// 親
+    /// 支点とコード
+    /// </summary>
+    public class PivotAndCodeIShortUIViewParent : MonoBehaviour
+    {
+        /// <summary>トランスフォーム</summary>
+        protected Transform _transform;
+    }
+}
