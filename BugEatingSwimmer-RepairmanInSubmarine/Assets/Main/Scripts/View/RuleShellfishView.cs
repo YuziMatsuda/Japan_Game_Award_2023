33dcf46c47be7be347815cb9a5bee2d4ba2ae8c0
ktgitfrog333 <paraysis+git@gmail.com>
2023-09-10 13:15:26 +0900

    ▼レベルデザイン 　●チュートリアル（ステージ１－１）の説明強化

--- BugEatingSwimmer-RepairmanInSubmarine/Assets/Main/Scripts/View/RuleShellfishView.cs
+++ BugEatingSwimmer-RepairmanInSubmarine/Assets/Main/Scripts/View/RuleShellfishView.cs
@@ -16,6 +16,12 @@ namespace Main.View
         [SerializeField] private BodySpriteRuleShellfish bodySpriteRuleShellfish;
         /// <summary>表示状態か</summary>
         public bool IsVisible => bodySpriteRuleShellfish.IsVisible;
+        /// <summary>吹き出しスプライト</summary>
+        [SerializeField] private BodySprite bodySprite;
+        /// <summary>表示</summary>
+        [SerializeField] private Color visibledColorOfChild = new Color(1f, 1f, 1f, 1f);
+        /// <summary>非表示</summary>
+        [SerializeField] private Color disabledColorOfChild = new Color(1f, 1f, 1f, 0f);
 
         public bool InstanceBubble()
         {
@@ -35,6 +41,8 @@ namespace Main.View
 
         public IEnumerator PlayChangeSpriteCloseBetweenOpen(System.IObserver<bool> observer)
         {
+            if (!SetColorSpriteRendererSpeechBubble(false))
+                Debug.LogError("吹き出しスプライトの表示／非表示呼び出しの失敗");
             Observable.FromCoroutine<bool>(observer => bodySpriteRuleShellfish.PlayChangeSpriteCloseBetweenOpen(observer))
                 .Subscribe(_ => observer.OnNext(true))
                 .AddTo(gameObject);
@@ -45,7 +53,12 @@ namespace Main.View
         public IEnumerator PlayChangeSpriteOpenBetweenClose(System.IObserver<bool> observer)
         {
             Observable.FromCoroutine<bool>(observer => bodySpriteRuleShellfish.PlayChangeSpriteOpenBetweenClose(observer))
-                .Subscribe(_ => observer.OnNext(true))
+                .Subscribe(_ =>
+                {
+                    observer.OnNext(true);
+                    if (!SetColorSpriteRendererSpeechBubble(true))
+                        Debug.LogError("吹き出しスプライトの表示／非表示呼び出しの失敗");
+                })
                 .AddTo(gameObject);
 
             yield return null;
@@ -53,17 +66,32 @@ namespace Main.View
 
         public bool SetColorSpriteIsVisible(bool isVisible)
         {
+            if (!SetColorSpriteRendererSpeechBubble(isVisible))
+                Debug.LogError("吹き出しスプライトの表示／非表示呼び出しの失敗");
             return ((IBodySpriteRuleShellfish)bodySpriteRuleShellfish).SetColorSpriteIsVisible(isVisible);
         }
 
         private void Reset()
         {
             bodySpriteRuleShellfish = GetComponentInChildren<BodySpriteRuleShellfish>();
+            bodySprite = transform.GetChild(1).GetComponent<BodySprite>();
+        }
+
+        /// <summary>
+        /// 吹き出しスプライトの表示／非表示
+        /// </summary>
+        /// <param name="isEnabled">表示／非表示</param>
+        /// <returns>成功／失敗</returns>
+        private bool SetColorSpriteRendererSpeechBubble(bool isEnabled)
+        {
+            return bodySprite.SetColorSpriteRenderer(isEnabled ? visibledColorOfChild : disabledColorOfChild);
         }
     }
 
     /// <summary>
-    /// 
+    /// ルール貝
+    /// ビュー
+    /// インターフェース
     /// </summary>
     public interface IRuleShellfishView
     {
