5b58d2e890d1cfc631d8d2b6412ed2abb6395874
ktgitfrog333 <paraysis+git@gmail.com>
2023-10-09 16:02:55 +0900

    ▼メカニクス 　●長時間プレイで画面がカクつく

--- BugEatingSwimmer-RepairmanInSubmarine/Assets/Main/Scripts/Presenter/MainPresenter.cs
+++ BugEatingSwimmer-RepairmanInSubmarine/Assets/Main/Scripts/Presenter/MainPresenter.cs
@@ -378,6 +378,9 @@ namespace Main.Presenter
                                 Debug.LogError("ボタン有効／無効切り替え呼び出しの失敗");
                             if (!gameProceedButtonModel.SetEventTriggerEnabled(false))
                                 Debug.LogError("イベント有効／無効切り替え呼び出しの失敗");
+                            // プレイヤーの挙動によって発生するイベント無効　など
+                            if (!MainGameManager.Instance.InputSystemsOwner.Exit())
+                                Debug.LogError("InputSystem終了呼び出しの失敗");
                             var owner = MainGameManager.Instance.SceneOwner;
                             if (!owner.SetSystemCommonCash(owner.CountUpSceneId(currentStageDic)))
                                 Debug.LogError("シーンID更新呼び出しの失敗");
@@ -420,6 +423,9 @@ namespace Main.Presenter
                                 Debug.LogError("ボタン有効／無効切り替え呼び出しの失敗");
                             if (!gameRetryButtonModel.SetEventTriggerEnabled(false))
                                 Debug.LogError("イベント有効／無効切り替え呼び出しの失敗");
+                            // プレイヤーの挙動によって発生するイベント無効　など
+                            if (!MainGameManager.Instance.InputSystemsOwner.Exit())
+                                Debug.LogError("InputSystem終了呼び出しの失敗");
                             // シーン読み込み時のアニメーション
                             Observable.FromCoroutine<bool>(observer => fadeImageView.PlayFadeAnimation(observer, EnumFadeState.Close))
                                 .Subscribe(_ => MainGameManager.Instance.SceneOwner.LoadMainScene())
@@ -459,6 +465,9 @@ namespace Main.Presenter
                                 Debug.LogError("ボタン有効／無効切り替え呼び出しの失敗");
                             if (!gameSelectButtonModel.SetEventTriggerEnabled(false))
                                 Debug.LogError("イベント有効／無効切り替え呼び出しの失敗");
+                            // プレイヤーの挙動によって発生するイベント無効　など
+                            if (!MainGameManager.Instance.InputSystemsOwner.Exit())
+                                Debug.LogError("InputSystem終了呼び出しの失敗");
                             // シーン読み込み時のアニメーション
                             Observable.FromCoroutine<bool>(observer => fadeImageView.PlayFadeAnimation(observer, EnumFadeState.Close))
                                 .Subscribe(_ =>
@@ -538,7 +547,7 @@ namespace Main.Presenter
                                     if (playerModel != null)
                                         if (!playerModel.SetInputBan(true))
                                             Debug.LogError("操作禁止フラグ更新呼び出しの失敗");
-                                    // T.B.D プレイヤーの挙動によって発生するイベント無効　など
+                                    // プレイヤーの挙動によって発生するイベント無効　など
                                     if (!MainGameManager.Instance.InputSystemsOwner.Exit())
                                         Debug.LogError("InputSystem終了呼び出しの失敗");
                                     // シーン読み込み時のアニメーション
@@ -1026,6 +1035,9 @@ namespace Main.Presenter
                                             // エラーSEを再生
                                             MainGameManager.Instance.AudioOwner.PlaySFX(ClipToPlay.se_code_error);
                                             Time.timeScale = 0f;
+                                            // プレイヤーの挙動によって発生するイベント無効　など
+                                            if (!MainGameManager.Instance.InputSystemsOwner.Exit())
+                                                Debug.LogError("InputSystem終了呼び出しの失敗");
                                             Observable.FromCoroutine<bool>(observer => fadeImageView.PlayFadeAnimation(observer, EnumFadeState.Close))
                                                 .Subscribe(_ => MainGameManager.Instance.SceneOwner.LoadMainScene())
                                                 .AddTo(gameObject);
@@ -1781,6 +1793,9 @@ namespace Main.Presenter
                                 // 実績履歴を更新
                                 if (common.AddMissionHistory() < 1)
                                     Debug.LogError("実績履歴を更新呼び出しの失敗");
+                                // プレイヤーの挙動によって発生するイベント無効　など
+                                if (!MainGameManager.Instance.InputSystemsOwner.Exit())
+                                    Debug.LogError("InputSystem終了呼び出しの失敗");
                                 var owner = MainGameManager.Instance.SceneOwner;
                                 if (!owner.SetSystemCommonCash(owner.CountUpSceneId(currentStageDic)))
                                     Debug.LogError("シーンID更新呼び出しの失敗");
